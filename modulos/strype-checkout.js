af945a0470b9644a31b4e43b3921426882d3e1ac
claudio996 <claudioantonio.retamal@gmail.com>
2022-01-09 14:49:27 -0300

    get products-prices in working template

--- modulos/strype-checkout.js
+++ modulos/strype-checkout.js
@@ -2,24 +2,42 @@ import KEYS from "./strype-keys.js"
 const d = document,
     $pastel = d.getElementById("pastel-choclo"),
     $template = d.getElementById("pastel-template").content,
-    $fragment = d.createDocumentFragment();
-
-fetch("https://api.stripe.com/v1/products", { //call api.
-    headers: {
-        Authorization: `Bearer ${KEYS.secret}`,
+    $fragment = d.createDocumentFragment(),
+    $fecthOption = {
+        headers: {
+            Authorization: `Bearer ${KEYS.secret}`,
+        }
     }
-}).then(resp => {
-    return resp.json()
-}).then(json => {
-    console.log(json);
-})
+let prices, products;
 
-fetch("https://api.stripe.com/v1/prices", { //call api.
-    headers: {
-        Authorization: `Bearer ${KEYS.secret}`,
-    }
-}).then(resp => {
-    return resp.json()
-}).then(json => {
-    console.log(json);
-})
+
+Promise.all([
+        fetch("https://api.stripe.com/v1/products", $fecthOption),
+        fetch("https://api.stripe.com/v1/prices", $fecthOption),
+    ])
+    .then(responses => Promise.all(responses.map(resp => resp.json()))) //map-> created new array with response in json.
+    .then(json => {
+        products = json[0].data;
+        prices = json[1].data;
+
+        console.log(products, prices);
+
+        //prommise products get name, image, from promise prices the prices and id.
+        prices.forEach(element => {
+            let productData = products.filter(product => product.id === element.product)
+            console.log(productData);
+            $template.querySelector('.pastel').setAttribute("data-prices", element.id);
+            $template.querySelector("img").src = productData[0].images[0];
+            let $clone = d.importNode($template, true);
+            $fragment.appendChild($clone);
+        });
+
+        $pastel.appendChild($fragment);
+
+    })
+    .catch(err => {
+
+        let message = err.statusText || "Ocurrio un error al conectarese con la api de stripe";
+        $pastel.innerHTML = `<p>Error ${err.status}: ${message} </p>`
+
+    });
