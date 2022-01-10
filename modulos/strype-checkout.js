87d13a11b6301848f5aad9467ea650125064d6ce
claudio996 <claudioantonio.retamal@gmail.com>
2022-01-10 12:46:45 -0300

    integration stripe checkout

--- modulos/strype-checkout.js
+++ modulos/strype-checkout.js
@@ -10,6 +10,9 @@ const d = document,
     }
 let prices, products;
 
+const moneyFormat = n => `$${n.slice(0,-3)}.`;
+
+const peso = number => (new Intl.NumberFormat("CL", { style: "currency", currency: "CLP" }).format(number));
 
 Promise.all([
         fetch("https://api.stripe.com/v1/products", $fecthOption),
@@ -20,7 +23,7 @@ Promise.all([
         products = json[0].data;
         prices = json[1].data;
 
-        console.log(products, prices);
+        console.log(prices);
 
         //prommise products get name, image, from promise prices the prices and id.
         prices.forEach(element => {
@@ -31,7 +34,7 @@ Promise.all([
             $template.querySelector("img").alt = productData[0].name;
             $template.querySelector("figcaption").innerHTML = `
             ${productData[0].name}<br>
-            $ ${element.unit_amount_decimal} ${element.currency}`
+            ${peso(element.unit_amount_decimal)} ${element.currency}`
 
             let $clone = d.importNode($template, true);
             $fragment.appendChild($clone);
@@ -46,3 +49,22 @@ Promise.all([
         $pastel.innerHTML = `<p>Error ${err.status}: ${message} </p>`
 
     });
+
+d.addEventListener("click", e => {
+    if (e.target.matches(".pastel *")) {
+        let price = e.target.parentElement.getAttribute("data-prices");
+        //console.log(priceId);
+        Stripe(KEYS.public).redirectToCheckout({
+            lineItems: [{ price, quantity: 1 }],
+            mode: "subscription",
+            successUrl: "http://127.0.0.1:5500/assets/stripe-success.html",
+            cancelUrl: "http://127.0.0.1:5500/assets/stripe-cancel.html",
+
+        }).then(res => {
+            if (res.error) {
+                console.log(res.error);
+                $pastel.insertAdjacentHTML("afterend", res.err.message);
+            }
+        })
+    }
+})
